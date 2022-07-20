5a5f2af9f968d7f5b45d719c4b977fde2f3ff71b
jomoonseok <moon97102002@gmail.com>
2022-07-20 09:17:01 +0900

    2022-07-20

---
+++ javastudy/01_VARIABLE/src/ex01/variable/Ex02.java
@@ -0,0 +1,18 @@
+package ex01.variable;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		// 문자열 타입
+		// 기본 타입(primitive type)은 아니다.
+		// 자주 사용하기 때문에 기본 타입처럼 사용할 수 있도록 자바가 지원해준다.
+		// String 클래스를 사용한다.
+		
+		String country = "korea"; // 문자는 한 글자 문자열은 두 글자 이상이다. / 자바가 쉬운 사용법을 지원 
+		String name = new String("민경태"); // 원래 사용법
+		System.out.println(country);
+		System.out.println(name);
+
+	}
+
+}
