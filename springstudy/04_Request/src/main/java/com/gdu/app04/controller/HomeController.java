901386b64eaea2d33100264a148f74bf608c7969
jomoonseok <moon97102002@gmail.com>
2022-10-31 17:49:53 +0900

    10/31 Spring 03_DI(Container에 등록된 Bean 가져오기) 04_Request(요청 처리)

---
+++ springstudy/04_Request/src/main/java/com/gdu/app04/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.gdu.app04.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public  String wlecome() {
+		return "index";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
