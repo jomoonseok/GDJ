72e3df6a6b70579c24ed97fb56d9071d877b5aec
jomoonseok <moon97102002@gmail.com>
2022-12-08 21:01:45 +0900

    12/08 파이널 프로젝트 + RESTful 보충

--- springstudy/REST/src/main/java/com/gdu/rest/controller/MemberRestController.java
+++ springstudy/REST/src/main/java/com/gdu/rest/controller/MemberRestController.java
@@ -1,11 +1,16 @@
 package com.gdu.rest.controller;
 
 import java.util.Map;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +27,10 @@ import com.gdu.rest.service.MemberService;
 	5. CRUD 처리 방식
                  URL        Method
 		1) 삽입 /members    POST
-		
+		2) 목록 /members    GET
+		3) 상세 /members/1  GET
+		4) 수정 /members    PUT // POST랑 똑같이 구현
+		5) 삭제 /members/1  DELETE
 */
 
 @RestController // 이 컨트롤러는 모든 메소드에 @ResponseBody 애너테이션을 추가한다.
@@ -36,5 +44,33 @@ public class MemberRestController {
 	public Map<String, Object> addMember(@RequestBody MemberDTO member, HttpServletResponse response){ // @RequestBody 요청 본문에서 찾는다.
 		return memberService.register(member, response);
 	}
+	
+	// 목록
+	@GetMapping(value="/members/page/{page}", produces="application/json")
+	public Map<String, Object> getMemberList(@PathVariable(value="page", required=false) Optional<String> opt){
+		int page = Integer.parseInt(opt.orElse("1"));
+		return memberService.getMemberList(page);
+	}
+	
+	// 상세
+	@GetMapping(value="/members/{memberNo}", produces="application/json")
+	public Map<String, Object> getMember(@PathVariable(value="memberNo", required=false) Optional<String> opt){
+		int memberNo = Integer.parseInt(opt.orElse("0"));
+		return memberService.getMemberByNo(memberNo);
+	}
+	
+	// 수정
+	@PutMapping(value="/members", produces="application/json")
+	public Map<String, Object> modifyMember(@RequestBody Map<String, Object> map, HttpServletResponse response){
+		//System.out.println(map);
+		return memberService.modifyMember(map, response);
+	}
+	
+	// 삭제
+	@DeleteMapping(value="/members/{memberNoList}", produces="application/json")
+	public Map<String, Object> removeMemberList(@PathVariable String memberNoList){
+		return memberService.removeMemberList(memberNoList);
+	}
+	
 	
 }
--- springstudy/REST/src/main/java/com/gdu/rest/service/MemverServiceImpl.java
+++ springstudy/REST/src/main/java/com/gdu/rest/service/MemverServiceImpl.java
@@ -1,16 +1,21 @@
 package com.gdu.rest.service;
 
+import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 import com.gdu.rest.domain.MemberDTO;
 import com.gdu.rest.mapper.MemberMapper;
+import com.gdu.rest.util.PageUtil;
 
 @Service
 public class MemverServiceImpl implements MemberService {
@@ -18,20 +23,121 @@ public class MemverServiceImpl implements MemberService {
 	@Autowired
 	private MemberMapper memberMapper;
 	
+	@Autowired
+	private PageUtil pageUtil;
+	
 	@Override
 	public Map<String, Object> register(MemberDTO member, HttpServletResponse response) {
 		// 어떤 예외가 오는지 확인하기 1. unique가 아닐 때 2. byte를 초과했을 때 3. not null인데 null값이 왔을 때
 		try {
 			
 			Map<String, Object> result = new HashMap<String, Object>();
-			result.put("insertREsult", memberMapper.insertMember(member));
+			result.put("insertResult", memberMapper.insertMember(member));
 			return result;
-		} catch(DuplicateKeyException e) {
-			
+		} catch(DuplicateKeyException e) { // 아이디 중복
+			try {
+				response.setContentType("text/html charset=UTF-8");
+				PrintWriter out = response.getWriter();
+				response.setStatus(501); // 응답 코드 501
+				out.println("이미 사용 중인 아이디 입니다.");
+				out.close();
+			} catch(Exception e2) {
+				e2.printStackTrace();
+			}
+		} catch(DataIntegrityViolationException e) { //DataIntegrityViolationException
+			try {
+				response.setContentType("text/html charset=UTF-8");
+				PrintWriter out = response.getWriter();
+				response.setStatus(502); // 응답 코드 502
+				out.println("필수 정보가 누락되었습니다.");
+				out.close();
+			} catch(Exception e2) {
+				e2.printStackTrace();
+			}
 		} catch(Exception e) {
-			System.out.println(e.getClass().getName());
+			try {
+				response.setContentType("text/html charset=UTF-8");
+				PrintWriter out = response.getWriter();
+				response.setStatus(503); // 응답 코드 503
+				out.println("입력 정보를 확인하세요.");
+				out.close();
+			} catch(Exception e2) {
+				e2.printStackTrace();
+			}
 		}
 		return null;
+	}
+	
+	
+	
+	@Override
+	public Map<String, Object> getMemberList(int page) {
+		
+		int totalRecord = memberMapper.selectMemberCount();
+		pageUtil.setPageUtil(page, totalRecord);
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("begin", pageUtil.getBegin());
+		map.put("end", pageUtil.getEnd());
+		
+		Map<String, Object> result = new HashMap<String, Object>();
+		result.put("memberList", memberMapper.selectMemberListByMap(map));
+		result.put("pageUtil", pageUtil);
+		
+		return result;
+	}
+	
+	
+	
+	@Override
+	public Map<String, Object> getMemberByNo(int memberNo) {
+		Map<String, Object> result = new HashMap<String, Object>();
+		result.put("member", memberMapper.selectMemberByNo(memberNo));
+		return result;
+	}
+	
+	
+	
+	@Override
+	public Map<String, Object> modifyMember(Map<String, Object> map, HttpServletResponse response) {
+		
+		try {
+			Map<String, Object> result = new HashMap<String, Object>();
+			result.put("updateResult", memberMapper.updateMember(map));
+			return result;
+		} catch(DataIntegrityViolationException e) {
+			try {
+				response.setContentType("text/plain; charset=UTF-8");
+				PrintWriter out = response.getWriter();
+				response.setStatus(501); // 응답코드
+				out.println("필수 정보가 누락되었습니다.");
+				out.close();
+			} catch(Exception e2) {
+				e2.printStackTrace();
+			}
+		} catch(Exception e) {
+			try {
+				response.setContentType("text/html charset=UTF-8");
+				PrintWriter out = response.getWriter();
+				response.setStatus(502); // 응답 코드 502
+				out.println("입력 정보를 확인하세요.");
+				out.close();
+			} catch(Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		
+		return null;
+	}
+	
+	
+	
+	@Override
+	public Map<String, Object> removeMemberList(String memberNoList) {
+		List<String> list = Arrays.asList(memberNoList.split(","));
+		Map<String, Object> result = new HashMap<String, Object>();
+		result.put("deleteResult", memberMapper.deleteMemberList(list));
+		return result;
 	}
 	
 }
